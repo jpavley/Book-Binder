839bdb06fa566649cbb0e835281fc7393c47b40d
John Pavley <jpavley@gmail.com>
2018-09-22 15:16:52 -0400

    added custom  collection view cell class

---
+++ Book Binder/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  Book Binder
+//
+//  Created by John Pavley on 9/22/18.
+//  Copyright © 2018 John Pavley. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var selectionImage: UIImageView!
+}
