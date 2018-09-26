57fbb69414a2ba376a240f5e8fe5a1f06493092e
John Pavley <jpavley@gmail.com>
2018-09-25 23:05:56 -0400

    massive terrible hack to get to MVP1!!!!

--- Book Binder/Controller/ViewController.swift
+++ Book Binder/Controller/ViewController.swift
@@ -14,23 +14,35 @@ class ViewController: UIViewController {
     @IBOutlet private weak var addButton: UIBarButtonItem!
     @IBOutlet private weak var deleteButton: UIBarButtonItem!
     
-    let columnCount = CGFloat(4)
+    let columnCount = CGFloat(6)
     let collectionViewMinSpacing = CGFloat(2)
     let cellHeight = CGFloat(40)
     
-    var collectionData = [
+    var collectionData1 = [
         "...", "583", "584", "585", "586", "587", "588",
         "589", "590", "591", "592", "593", "594", "595",
         "596", "597", "598", "599", "600", "601", "602",
         "603", "604", "605", "606", "607", "608", "+"
     ]
     
+    var collectionData2 = [
+        "...", "1v", "2", "3", "4", "+"
+    ]
+    
+    var collectionData3 = [
+        "...", "45", "46", "47", "48", "49", "50",
+        "51", "52", "53", "54", "55", "56", "57",
+        "58", "59", "60", "61", "62", "63", "64",
+        "65", "66", "67", "68", "69", "70", "+",
+    ]
+
+    
     @IBAction func addItem() {
         
         // add one item per touch
         
         collectionView.performBatchUpdates({
-            addItems(itemList: ["\(collectionData.count + 1) 😈"])
+            // addItems(itemList: ["\(collectionData.count + 1) 😈"])
         }, completion: nil)
     }
     
@@ -45,7 +57,7 @@ class ViewController: UIViewController {
             // beginning of the visible items.
             let items = selected.map { $0.item }.sorted().reversed()
             for item in items {
-                collectionData.remove(at: item)
+                // collectionData.remove(at: item)
             }
             
             collectionView.deleteItems(at: selected)
@@ -78,6 +90,7 @@ class ViewController: UIViewController {
         // - Toggles the button between Edit and Done
         // - Toggles the view controller between editing and not-editing mode
         navigationItem.leftBarButtonItem = editButtonItem
+        navigationItem.leftBarButtonItem?.isEnabled = false
         navigationController?.isToolbarHidden = true
     }
     
@@ -124,7 +137,7 @@ class ViewController: UIViewController {
         // ad more than one item per pull
         
         collectionView.performBatchUpdates({
-            addItems(itemList: ["\(collectionData.count + 1) 👽", "\(collectionData.count + 2) 💩"])
+            //addItems(itemList: ["\(collectionData.count + 1) 👽", "\(collectionData.count + 2) 💩"])
         }, completion: nil)
         
         collectionView.refreshControl?.endRefreshing()
@@ -141,8 +154,20 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
             
             let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "HeaderView", for: indexPath) as! CollectionReusableView
             
-            headerView.titleLabel.text = "Daredevil 2018"
-            headerView.subTitleLabel.text = "Marvel Entertainment, Comicbook"
+            if indexPath.section == 0 {
+                headerView.titleLabel.text = "Daredevil 2018"
+                headerView.subTitleLabel.text = "Marvel Entertainment, Comicbook"
+            }
+            
+            if indexPath.section == 1  {
+                headerView.titleLabel.text = "Doctor Strange 2018"
+                headerView.subTitleLabel.text = "Marvel Entertainment, Comicbook"
+            }
+            
+            if indexPath.section == 2 {
+                headerView.titleLabel.text = "ROM Spaceknight 1979"
+                headerView.subTitleLabel.text = "Marvel Comics Group, Comicbook"
+            }
             
             return headerView
             
@@ -154,36 +179,104 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView,
                         numberOfItemsInSection section: Int) -> Int {
         
-        return collectionData.count
+        if section == 0 {
+            return collectionData1.count
+        }
+        
+        if section == 1 {
+            return collectionData2.count
+        }
+
+        if section == 2 {
+            return collectionData3.count
+        }
+        
+        return 0
+
+        //return collectionData.count
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
     }
     
     func collectionView(_ collectionView: UICollectionView,
                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell",
-                                                      for: indexPath) as! CollectionViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
         
-        
-        let blueStrings = ["...", "605", "606", "607", "608", "+"]
-        
-        if blueStrings.contains(collectionData[indexPath.row]) {
+        if indexPath.section == 0 {
             
-            let attributes: [NSAttributedString.Key: Any] = [
-                NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17),
-                NSAttributedString.Key.foregroundColor: UIColor.blue
-            ]
+            let blueStrings = ["...", "605", "606", "607", "608", "+"]
             
-            cell.titleLabel.attributedText = NSAttributedString(string: collectionData[indexPath.row], attributes: attributes)
-        } else {
-            
-            let attributes: [NSAttributedString.Key: Any] = [
-                NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17),
-                NSAttributedString.Key.foregroundColor: UIColor.red,
-                NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue
-            ]
-            
-            cell.titleLabel.attributedText = NSAttributedString(string: collectionData[indexPath.row], attributes: attributes)
+            if blueStrings.contains(collectionData1[indexPath.row]) {
+                
+                let attributes: [NSAttributedString.Key: Any] = [
+                    NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17),
+                    NSAttributedString.Key.foregroundColor: UIColor.blue
+                ]
+                
+                cell.titleLabel.attributedText = NSAttributedString(string: collectionData1[indexPath.row], attributes: attributes)
+            } else {
+                
+                let attributes: [NSAttributedString.Key: Any] = [
+                    NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17),
+                    NSAttributedString.Key.foregroundColor: UIColor.red,
+                    NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue
+                ]
+                
+                cell.titleLabel.attributedText = NSAttributedString(string: collectionData1[indexPath.row], attributes: attributes)
+            }
         }
+        
+        if indexPath.section == 1 {
+            
+            let blueStrings = ["...", "1v", "3", "4", "+"]
+            
+            if blueStrings.contains(collectionData2[indexPath.row]) {
+                
+                let attributes: [NSAttributedString.Key: Any] = [
+                    NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17),
+                    NSAttributedString.Key.foregroundColor: UIColor.blue
+                ]
+                
+                cell.titleLabel.attributedText = NSAttributedString(string: collectionData2[indexPath.row], attributes: attributes)
+            } else {
+                
+                let attributes: [NSAttributedString.Key: Any] = [
+                    NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17),
+                    NSAttributedString.Key.foregroundColor: UIColor.red,
+                    NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue
+                ]
+                
+                cell.titleLabel.attributedText = NSAttributedString(string: collectionData2[indexPath.row], attributes: attributes)
+            }
+        }
+        
+        if indexPath.section == 2 {
+            
+            let blueStrings = ["...", "49", "52", "55", "57", "58", "59", "63", "69", "70", "+"]
+            
+            if blueStrings.contains(collectionData3[indexPath.row]) {
+                
+                let attributes: [NSAttributedString.Key: Any] = [
+                    NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17),
+                    NSAttributedString.Key.foregroundColor: UIColor.blue
+                ]
+                
+                cell.titleLabel.attributedText = NSAttributedString(string: collectionData3[indexPath.row], attributes: attributes)
+            } else {
+                
+                let attributes: [NSAttributedString.Key: Any] = [
+                    NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17),
+                    NSAttributedString.Key.foregroundColor: UIColor.red,
+                    NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue
+                ]
+                
+                cell.titleLabel.attributedText = NSAttributedString(string: collectionData3[indexPath.row], attributes: attributes)
+            }
+        }
+
         
         
         cell.isEditing = isEditing
@@ -209,8 +302,20 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
             performSegue(withIdentifier: "DetailSegue", sender: indexPath)
         }
         
-        let text = collectionData[indexPath.row]
-        print("selected \(text)")
+        if indexPath.section == 0 {
+            let text = collectionData1[indexPath.row]
+            print(text)
+        }
+        
+        if indexPath.section == 1 {
+            let text = collectionData2[indexPath.row]
+            print(text)
+        }
+        
+        if indexPath.section == 2 {
+            let text = collectionData3[indexPath.row]
+            print(text)
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -224,7 +329,17 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
         if let dest = segue.destination as? DetailViewController,
             let index = sender as? IndexPath {
             
-            dest.selection = collectionData[index.row]
+            if index.section == 0 {
+                dest.selection = collectionData1[index.row]
+            }
+            
+            if index.section == 1 {
+                dest.selection = collectionData2[index.row]
+            }
+            
+            if index.section == 2 {
+                dest.selection = collectionData3[index.row]
+            }
         }
     }
     
@@ -246,12 +361,12 @@ extension ViewController {
     }
     
     func addItemToModel(item: String) {
-        collectionData.append(item)
+        //collectionData.append(item)
     }
     
     func updateCollectionView() {
-        let indexPath = IndexPath(row: collectionData.count - 1, section: 0)
-        collectionView.insertItems(at: [indexPath])
+//        let indexPath = IndexPath(row: collectionData.count - 1, section: 0)
+//        collectionView.insertItems(at: [indexPath])
     }
 }
 
