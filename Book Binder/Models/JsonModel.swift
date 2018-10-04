e1f9b3bb7d2180a3bfe17fc128400428b71c5e47
John Pavley <jpavley@gmail.com>
2018-10-04 08:49:56 -0400

    added JSON coding struct which will be used to translate JSON data into Swift objects

---
+++ Book Binder/Models/JsonModel.swift
@@ -0,0 +1,33 @@
+//
+//  JsonModel.swift
+//  Book Binder
+//
+//  Created by John Pavley on 10/4/18.
+//  Copyright © 2018 John Pavley. All rights reserved.
+//
+
+import Foundation
+
+struct JasonComicbook: Codable {
+    
+    struct JsonSeries: Codable {
+        let seriesPublisher: String
+        let seriesTitle: String
+        let seriesEra: Int
+        let seriesFirstIssue: Int
+        let seriesCurrentIssue: Int
+        let seriesSkippedIssues: Int
+        let seriesExtraIssues: Int
+    }
+    
+    struct JsonBook: Codable {
+        let issueNumber: Int
+        let variantLetter: String
+        let isOwned: Bool
+    }
+    
+    let series: JsonSeries
+    let books: [JsonBook]
+}
+
+
