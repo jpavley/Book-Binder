97c866027df98277037861df63287886e801f1dc
John Pavley <jpavley@gmail.com>
2018-10-04 23:21:08 -0400

    tested decoding an array of series

--- Book Binder Model Tests/JsonModel_Tests.swift
+++ Book Binder Model Tests/JsonModel_Tests.swift
@@ -9,15 +9,15 @@
 import XCTest
 
 class JsonModel_Tests: XCTestCase {
-
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testCreateJson() {
         let jsonString = """
             {
@@ -38,6 +38,7 @@ class JsonModel_Tests: XCTestCase {
                  ]
             }
         """
+        
         let jsonData = jsonString.data(using: .utf8)!
         let decoder = JSONDecoder()
         let jsonModel = try! decoder.decode(JsonModel.self, from: jsonData)
@@ -57,6 +58,60 @@ class JsonModel_Tests: XCTestCase {
         XCTAssertEqual(book.issueNumber, 605)
         XCTAssertEqual(book.variantLetter, "")
         XCTAssertEqual(book.isOwned, true)
-   }
-
+    }
+    
+    func testCreateSeriesArray() {
+        let jsonString = """
+            [{
+                "seriesPublisher": "Marvel Entertainment",
+                "seriesTitle": "Daredevil",
+                "seriesEra": 2017,
+                "seriesFirstIssue": 595,
+                "seriesCurrentIssue": 608,
+                "seriesSkippedIssues": 0,
+                "seriesExtraIssues": 0,
+                "books":
+                [
+                    {
+                        "issueNumber": 605,
+                        "variantLetter": "",
+                        "isOwned": true
+                    }
+                 ]
+            },
+            {
+                "seriesPublisher": "DC Comics",
+                "seriesTitle": "Superman",
+                "seriesEra": 2000,
+                "seriesFirstIssue": 100,
+                "seriesCurrentIssue": 1000,
+                "seriesSkippedIssues": 2,
+                "seriesExtraIssues": 10,
+                "books":
+                [
+                    {
+                        "issueNumber": 123,
+                        "variantLetter": "c",
+                        "isOwned": false
+                    },
+                    {
+                        "issueNumber": 345,
+                        "variantLetter": "e",
+                        "isOwned": true
+                    }
+                 ]
+            }]
+        """
+        
+        let jsonData = jsonString.data(using: .utf8)!
+        let decoder = JSONDecoder()
+        let jsonModel = try! decoder.decode([JsonModel].self, from: jsonData)
+        
+        XCTAssertNotNil(jsonModel)
+        XCTAssertEqual(jsonModel.count, 2)
+        XCTAssertEqual(jsonModel[1].seriesTitle, "Superman")
+        XCTAssertEqual(jsonModel[1].books[1].issueNumber, 345)
+    }
+    
+    
 }
