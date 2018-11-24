ef9f047cf3adb48ee1a1fd43f1dbe4af2a680883
John Pavley <john@pavley.com>
2018-11-24 13:47:11 -0500

    named color assets for section rules

--- Book Binder/Views/CollectionReusableView.swift
+++ Book Binder/Views/CollectionReusableView.swift
@@ -13,16 +13,41 @@ class CollectionReusableView: UICollectionReusableView {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var subTitleLabel: UILabel!
     
-    override func draw(_ rect: CGRect) {
-        
+    func drawRule(start: CGPoint, end: CGPoint, color: CGColor, width: CGFloat) {
         if let context = UIGraphicsGetCurrentContext() {
-            context.move(to: CGPoint(x: rect.minX, y: rect.minY))
-            context.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
-            context.setStrokeColor(UIColor.lightGray.cgColor)
-            context.setLineWidth(1.0)
-            //context.setLineDash(phase: 5.0, lengths: [5.0, 5.0])
+            context.move(to: CGPoint(x: start.x, y: start.y))
+            context.addLine(to: CGPoint(x: end.x, y: end.y))
+            context.setStrokeColor(color)
+            context.setLineWidth(width)
             context.strokePath()
             
         }
+    }
+    
+    override func draw(_ rect: CGRect) {
+        
+        let start1 = CGPoint(x: rect.minX, y: rect.minY)
+        let end1 = CGPoint(x: rect.maxX, y: rect.minY)
+        let color1 = UIColor(named: "SectionColorDark")!.cgColor
+        let width1 = CGFloat(2.0)
+        
+        
+        let start2 = CGPoint(x: rect.minX, y: rect.minY+2)
+        let end2 = CGPoint(x: rect.maxX, y: rect.minY+2)
+        let color2 = UIColor(named: "SectionColorLight")!.cgColor
+        let width2 = CGFloat(4.0)
+        
+        drawRule(start: start2, end: end2, color: color2, width: width2)
+        drawRule(start: start1, end: end1, color: color1, width: width1)
+
+        
+//        if let context = UIGraphicsGetCurrentContext() {
+//            context.move(to: CGPoint(x: rect.minX, y: rect.minY))
+//            context.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+//            context.setStrokeColor(UIColor.lightGray.cgColor)
+//            context.setLineWidth(1.0)
+//            context.strokePath()
+//
+//        }
     }
 }
