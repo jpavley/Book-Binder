fa37f7a047d854adb02932db7b69a3581d4931b6
John Pavley <john@pavley.com>
2018-12-11 08:32:32 -0500

    added PopoverViewManaager

---
+++ Book Binder/Controllers/PopoverViewManager.swift
@@ -0,0 +1,13 @@
+//
+//  PopoverViewManager.swift
+//  Book Binder
+//
+//  Created by John Pavley on 12/11/18.
+//  Copyright © 2018 John Pavley. All rights reserved.
+//
+
+import Foundation
+
+class PopoverViewManager {
+    
+}
