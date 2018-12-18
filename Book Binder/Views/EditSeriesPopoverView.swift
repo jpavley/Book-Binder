c253bc389290d73f82a1b08a578f12ebc03cc230
John Pavley <john@pavley.com>
2018-12-17 19:26:29 -0500

    wiring up the edit series popover view

--- Book Binder/Views/EditSeriesPopoverView.swift
+++ Book Binder/Views/EditSeriesPopoverView.swift
@@ -10,4 +10,23 @@ import UIKit
 
 class EditSeriesPopoverView: UIView {
     
+    @IBOutlet weak var ESVPublisherTextField: UITextField!
+    @IBOutlet weak var ESVSeriesTextField: UITextField!
+    @IBOutlet weak var ESVEraTextField: UITextField!
+    @IBOutlet weak var ESVCoverImageView: UIImageView!
+    
+    @IBAction func photosAction(_ sender: Any) {
+    }
+    
+    @IBAction func cameraAction(_ sender: Any) {
+    }
+    
+    @IBAction func noImageAction(_ sender: Any) {
+    }
+    
+    @IBAction func doneAction(_ sender: Any) {
+    }
+    
+    @IBAction func cancelAction(_ sender: Any) {
+    }
 }
