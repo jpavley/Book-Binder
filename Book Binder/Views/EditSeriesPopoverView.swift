71287db2957043bc32bbc588930acb72bf696868
John Pavley <john@pavley.com>
2018-12-17 23:17:15 -0500

    improved outlet names

--- Book Binder/Views/EditSeriesPopoverView.swift
+++ Book Binder/Views/EditSeriesPopoverView.swift
@@ -10,23 +10,28 @@ import UIKit
 
 class EditSeriesPopoverView: UIView {
     
-    @IBOutlet weak var ESVPublisherTextField: UITextField!
-    @IBOutlet weak var ESVSeriesTextField: UITextField!
-    @IBOutlet weak var ESVEraTextField: UITextField!
-    @IBOutlet weak var ESVCoverImageView: UIImageView!
+    @IBOutlet weak var publisherTextField: UITextField!
+    @IBOutlet weak var seriesTextField: UITextField!
+    @IBOutlet weak var eraTextField: UITextField!
+    @IBOutlet weak var coverImageView: UIImageView!
     
     @IBAction func photosAction(_ sender: Any) {
+        
     }
     
     @IBAction func cameraAction(_ sender: Any) {
+        
     }
     
     @IBAction func noImageAction(_ sender: Any) {
+        
     }
     
     @IBAction func doneAction(_ sender: Any) {
+        
     }
     
     @IBAction func cancelAction(_ sender: Any) {
+        
     }
 }
