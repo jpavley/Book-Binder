346d22536767a6606d830eb983dc9f75a3e96798
John Pavley <john@pavley.com>
2019-01-02 11:51:55 -0500

    created EditIssuePopoverView: PopoverUIView and organized all popover views into a folder

---
+++ Book Binder/Views/PopOverViews/EditIssuePopoverView.swift
@@ -0,0 +1,13 @@
+//
+//  EditIssuePopoverView.swift
+//  Book Binder
+//
+//  Created by John Pavley on 1/2/19.
+//  Copyright © 2019 John Pavley. All rights reserved.
+//
+
+import UIKit
+
+class EditIssuePopoverView: PopoverUIView {
+
+}
