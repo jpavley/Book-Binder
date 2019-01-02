2e6edb6c8580a6d9c8aff3728e34958f086214ec
John Pavley <john@pavley.com>
2019-01-02 13:48:21 -0500

    AddIssuePopoverView is now a subclass of PopoverUIView

--- Book Binder/Views/PopOverViews/AddIssuePopoverView.swift
+++ Book Binder/Views/PopOverViews/AddIssuePopoverView.swift
@@ -8,7 +8,7 @@
 
 import UIKit 
 
-class AddIssuePopoverView: UIView {
+class AddIssuePopoverView: PopoverUIView {
     
     @IBOutlet weak var issueNumberField: UITextField!
     @IBOutlet weak var variantLetterField: UITextField!
@@ -18,20 +18,16 @@ class AddIssuePopoverView: UIView {
     @IBOutlet weak var seriesTitleLabel: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
     
-    var saveFunction: () -> () = { print("series changes saved") }
-    var cancelFunction: () -> () = { print("series changes canceled") }
-    var deleteFunction: () -> () = { print("series deleted") }
-    
     var comicBookCollection: JsonModel!
     
     @IBAction func doneAction(_ sender: Any) {
         saveFunction()
-        exitPopoverView(popoverView: self, visualEffectView: visualEffectView)
+        exitPopoverView(visualEffectView: visualEffectView)
     }
     
     @IBAction func cancelAction(_ sender: Any) {
         cancelFunction()
-        exitPopoverView(popoverView: self, visualEffectView: visualEffectView)
+        exitPopoverView(visualEffectView: visualEffectView)
     }
     
     @IBAction func photosAction(_ sender: Any) {
